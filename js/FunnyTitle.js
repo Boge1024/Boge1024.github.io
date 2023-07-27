01bba59415cb77fe8d8fa03e60e2972dc8c2039a
Boge1024 <414278117@qq.com>
2023-07-27 16:20:58 +0800

    Site updated: 2023-07-27 16:20:57

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,17 +1,17 @@
 <!--浏览器搞笑标题-->
- var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         // $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '我已经藏好了~';
-         clearTimeout(titleTime);
-     }
-     else {
-         // $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '你终于找到我了！';
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 10);
-     }
- });
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/funny.ico");
+        document.title = '我已经藏好了~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '你终于找到我了！' + ' | ' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 100);
+    }
+});
